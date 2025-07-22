a3fd83fd1d97e98b4665a1a44c7bff57ae609357
PinkPanteRus <pinkpanterus@gmail.com>
2025-07-22 21:40:58 +0300

    ДЗ по уроку 10 (Строки). Сделано первое задание.

---
+++ ru.rtk.java/src/Homework_4/HW_4_1.java
@@ -0,0 +1,18 @@
+package Homework_4;
+
+import java.util.Scanner;
+
+public class HW_4_1 {
+    public static void main(String[] args){
+        final String keyboard = "qwertyuiopasdfghjklzxcvbnm";
+        System.out.println("Введите одну букву латинского алфавита");
+        Scanner scanner = new Scanner(System.in);
+        String string = scanner.nextLine();
+
+        int index = keyboard.indexOf(string.toLowerCase().charAt(0));
+        int prevIndex = (index - 1 + keyboard.length()) % keyboard.length();
+        char ch = keyboard.charAt(prevIndex);
+        System.out.printf("Буква слева от заданной буквы %s! %n", ch);
+    }
+
+}
