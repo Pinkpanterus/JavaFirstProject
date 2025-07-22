8dd43bfcda26fbe0c7df7e73cbe61e30b2f12045
PinkPanteRus <pinkpanterus@gmail.com>
2025-07-22 22:18:27 +0300

    ДЗ по уроку 10 (Строки). Сделано первое, второе и третье задания.

---
+++ ru.rtk.java/src/Homework_4/HW_4_3.java
@@ -0,0 +1,25 @@
+package Homework_4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class HW_4_3 {
+    public static void main(String[] args){
+        System.out.println("Введите строку из двух слов на латинице:");
+        Scanner scanner = new Scanner(System.in);
+        String enteredString = scanner.nextLine();
+
+        String[] splitedStrings = enteredString.split(" ");
+        StringBuilder sb = new StringBuilder(enteredString.length());
+        for (String string: splitedStrings){
+            char[] chars = string.toCharArray();
+            Arrays.sort(chars);
+            sb.append(chars);
+
+            if (sb.length() < enteredString.length()) {
+                sb.append(' ');
+            }
+        }
+        System.out.printf("Отсортированные символы: %s %n", sb);
+    }
+}
